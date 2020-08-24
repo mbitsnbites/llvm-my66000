9e2d55a3477e1df5b35ec13b0fa067965f124387
Brian G. Lucas <bgl@tlanet.org>
2020-08-24 11:53:45 -0500

    Implement clang support.  Stuff shamelessly copied from Le64 support.

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -36,6 +36,7 @@
 #include "Targets/WebAssembly.h"
 #include "Targets/X86.h"
 #include "Targets/XCore.h"
+#include "Targets/My66000.h"
 #include "clang/Basic/Diagnostic.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/Triple.h"
@@ -590,6 +591,9 @@ TargetInfo *AllocateTarget(const llvm::Triple &Triple,
     return new LinuxTargetInfo<RenderScript32TargetInfo>(Triple, Opts);
   case llvm::Triple::renderscript64:
     return new LinuxTargetInfo<RenderScript64TargetInfo>(Triple, Opts);
+
+  case llvm::Triple::my66000:
+    return new My66000TargetInfo(Triple, Opts);
   }
 }
 } // namespace targets
